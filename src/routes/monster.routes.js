c1b791c037fa896aa6dd7a6298ad59fdddfb0641
Gary <kululu.gary@gmail.com>
2020-05-16 18:56:51 +0200

    npc put

--- src/routes/monster.routes.js
+++ src/routes/monster.routes.js
@@ -42,7 +42,7 @@ router.get('/bestiary/:id', async (req, res) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, secret.key);
-        
+
         if (decoded) {
             const monster = await Monster.findById(req.params.id);
 
@@ -57,12 +57,46 @@ router.get('/bestiary/:id', async (req, res) => {
                 message: "No JWT"
             })
         }
-        
-    } catch(e) {
+
+    } catch (e) {
         res.json({
             status: 500,
             message: "Internal server error"
         })
+    }
+})
+
+router.post('/bestiary', async (req, res) => {
+    try {
+        const monster = req.body;
+        const newMonster = new Monster(monster);
+        newMonster.save(function(err) {
+            if (err) {
+                return res.status(400).status({
+                    message: "El monstruo ya existe."
+                })
+            }
+        })
+            .then(() => res.json({ status: 200, message: "Monstruo añadido" }))
+            .catch(err => res.status(400).json('Error: ' + err))
+    } catch (e) { 
+        res.status(400).send('El monstruo no ha podido ser añadido.')
+    }
+})
+
+router.put('/bestiary', async (req, res) => {
+    try {
+        const monster = req.body;
+        // const newMonster = new Monster(monster);
+        monster.save(function(err) {
+            if (err) {
+                res.status(400).send('El monstruo no ha podido ser añadido.')
+            }
+        })
+            .then(() => res.json({ status: 200, message: "Monstruo añadido" }))
+            .catch(err => res.status(400).json('Error: ' + err))
+    } catch (e) { 
+        res.status(400).send('El monstruo no ha podido ser añadido.')
     }
 })
 
