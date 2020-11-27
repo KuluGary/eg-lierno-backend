257603b6419686c1df2b90bd94badde3a7058e50
Gary <kululu.gary@gmail.com>
2020-11-27 17:19:31 +0100

    removed config file

--- src/configs/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    key: "miclaveultrasecreta123*",
-    discordBotToken: 'NzY0NTgxMTczMTE1ODc5NDI1.X4IVtA.3V6n-auTV74Jx_n5nqG97B0eVqA',
-    mongo_db: {
-        development: "lierno",
-        test: "test-lierno"
-    }
-}
